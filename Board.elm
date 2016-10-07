8e1d7bcc6af04035dc2b2efb08dda86aa298f642
Bill St. Clair <billstclair@gmail.com>
2016-10-07 19:45:34 -0400

    Header comments for Board.elm

--- Board.elm
+++ Board.elm
@@ -1,3 +1,14 @@
+----------------------------------------------------------------------
+--
+-- Board.elm
+-- Rectangular game board with integer elements
+-- Copyright (c) 2016 Bill St. Clair <billstclair@gmail.com>
+-- Some rights reserved.
+-- Distributed under the MIT License
+-- See LICENSE.txt
+--
+----------------------------------------------------------------------
+
 module Board exposing
   ( Board, make, get, set, getRow
   )
