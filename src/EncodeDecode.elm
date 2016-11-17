8a197950451b5590f5a06957baad361d31c69a8f
Bill St. Clair <billstclair@gmail.com>
2016-11-16 20:32:01 -0500

    Decoders done.

--- src/EncodeDecode.elm
+++ src/EncodeDecode.elm
@@ -1,50 +1,27 @@
-import SharedTypes exposing ( GameState, SavedModel
-                            , Labels, Hints, Flags, Selection
+----------------------------------------------------------------------
+--
+-- EncodeDecode.elm
+-- Functions to encode state to JSON strings and decode it back into Elm
+-- Copyright (c) 2016 Bill St. Clair <billstclair@gmail.com>
+-- Some rights reserved.
+-- Distributed under the MIT License
+-- See LICENSE.txt
+--
+----------------------------------------------------------------------
+
+module EncodeDecode exposing (decodeGameState0, decodeSavedModel0)
+
+import SharedTypes exposing ( Labels, Hints, Flags, Selection
                             , IntBoard, LabelsBoard, HintsBoard
                             )
 import Board exposing (Board)
 
-import Html exposing (Html, div, text)
 import Json.Decode as JD exposing (field, Decoder)
 import Json.Encode as JE
+import Time exposing (Time)
 
 type alias Model =
     Int
-
-main =
-    Html.beginnerProgram
-        { model = 1
-        , view = view
-        , update = update
-        }
-
-update : msg -> model -> model
-update msg model =
-    model
-
-br : Html msg
-br =
-    Html.br [][]
-
-view : model -> Html msg
-view model =
-    div []
-        [ text <| "hash: " ++ (toString (decodeGameState hash))
-        , br
-        , text <| "kakuroDojo: " ++ kakuroDojo
-        ]
-
-hash : String
-hash =
-    "{\"version\":7,\"board\":{\"rows\":6,\"cols\":6,\"defaultValue\":0,\"spec\":\".38.31.13.12..139..798..31.92.92.21.\",\"index\":1,\"array\":[[0,3,8,0,3,1],[0,1,3,0,1,2],[0,0,1,3,9,0],[0,7,9,8,0,0],[3,1,0,9,2,0],[9,2,0,2,1,0]]},\"labels\":{\"rows\":7,\"cols\":7,\"defaultValue\":[0,0],\"spec\":null,\"index\":null,\"array\":[[[0,0],[0,0],[0,4],[0,21],[0,0],[0,13],[0,3]],[[0,0],[11,0],[0,0],[0,0],[4,0],[0,0],[0,0]],[[0,0],[4,0],[0,0],[0,0],[3,22],[0,0],[0,0]],[[0,0],[0,0],[13,10],[0,0],[0,0],[0,0],[0,0]],[[0,0],[24,12],[0,0],[0,0],[0,0],[0,3],[0,0]],[[4,0],[0,0],[0,0],[11,0],[0,0],[0,0],[0,0]],[[11,0],[0,0],[0,0],[3,0],[0,0],[0,0],[0,0]]]},\"allDone\":false,\"guesses\":{\"rows\":6,\"cols\":6,\"defaultValue\":0,\"spec\":null,\"index\":null,\"array\":[[0,3,8,0,3,1],[0,1,3,0,1,2],[0,0,1,3,9,0],[0,7,9,8,0,0],[3,1,0,9,2,0],[9,2,0,2,1,0]]},\"hints\":{\"rows\":6,\"cols\":6,\"defaultValue\":[],\"spec\":null,\"index\":null,\"array\":[[[],[],[],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[],[]]]},\"flags\":{\"isHintInput\":false,\"showPossibilities\":true},\"selection\":[2,2]}"
-
-kakuroDojo: String
-kakuroDojo =
-    "{\"version\":7,\"kind\":6,\"index\":3,\"gencount\":2,\"gameState\":{\"version\":7,\"board\":{\"rows\":6,\"cols\":6,\"defaultValue\":0,\"spec\":\".24.98.96185..72....84..14938.39.59.\",\"index\":3,\"array\":[[0,2,4,0,9,8],[0,9,6,1,8,5],[0,0,7,2,0,0],[0,0,8,4,0,0],[1,4,9,3,8,0],[3,9,0,5,9,0]]},\"labels\":{\"rows\":7,\"cols\":7,\"defaultValue\":[0,0],\"spec\":null,\"index\":null,\"array\":[[[0,0],[0,0],[0,11],[0,34],[0,0],[0,17],[0,13]],[[0,0],[6,0],[0,0],[0,0],[17,15],[0,0],[0,0]],[[0,0],[29,0],[0,0],[0,0],[0,0],[0,0],[0,0]],[[0,0],[0,0],[9,0],[0,0],[0,0],[0,0],[0,0]],[[0,0],[0,4],[12,13],[0,0],[0,0],[0,17],[0,0]],[[25,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],[[12,0],[0,0],[0,0],[14,0],[0,0],[0,0],[0,0]]]},\"allDone\":false,\"guesses\":{\"rows\":6,\"cols\":6,\"defaultValue\":0,\"spec\":null,\"index\":null,\"array\":[[0,2,4,0,9,8],[0,9,6,1,8,5],[0,0,7,2,0,0],[0,0,8,4,0,0],[0,0,9,3,8,0],[0,0,0,5,9,0]]},\"hints\":{\"rows\":6,\"cols\":6,\"defaultValue\":[],\"spec\":null,\"index\":null,\"array\":[[[],[],[],[],[9,8],[]],[[],[],[],[],[9,8],[]],[[],[],[7,6,5],[4,3,2],[],[]],[[],[],[8,9],[4,3],[],[]],[[1,3],[],[9,8],[],[9,8],[]],[[1,3],[],[],[6,5],[9,8],[]]]},\"flags\":{\"isHintInput\":false,\"showPossibilities\":true},\"selection\":[4,3]},\"time\":0}"
-
-version0 : Int
-version0 =
-    7
 
 xBoardDecoder : Decoder x -> Decoder (Board x)
 xBoardDecoder xDecoder =
@@ -86,20 +63,6 @@ hintsBoardDecoder : Decoder HintsBoard
 hintsBoardDecoder =
     xBoardDecoder hintsDecoder
 
-{-
-    { version : Int --modelVersion
-    , board : IntBoard
-    , labels : LabelsBoard
-    , allDone : Bool
-    , guesses : IntBoard
-    , hints : HintsBoard
-    , flags : Flags
-    , selection : Maybe Selection
-
-decodeGameState0 : String -> GameState
-decodeGameState json =
--}
-    
 flagsDecoder : Decoder Flags
 flagsDecoder =
     JD.map2
@@ -118,10 +81,21 @@ maybeSelectionDecoder =
     JD.nullable (JD.list JD.int)
         |> JD.map listToMaybeSelection
 
-gameStateDecoder : Decoder GameState
+type alias GameState0 =
+    { version : Int --modelVersion
+    , board : IntBoard
+    , labels : LabelsBoard
+    , allDone : Bool
+    , guesses : IntBoard
+    , hints : HintsBoard
+    , flags : Flags
+    , selection : Maybe Selection
+    }
+
+gameStateDecoder : Decoder GameState0
 gameStateDecoder =
     JD.map8
-        GameState
+        GameState0
         (field "version" JD.int)
         (field "board" intBoardDecoder)
         (field "labels" labelsBoardDecoder)
@@ -131,6 +105,52 @@ gameStateDecoder =
         (field "flags" flagsDecoder)
         (field "selection" maybeSelectionDecoder)
 
-decodeGameState : String -> Result String GameState
-decodeGameState json =
-    JD.decodeString gameStateDecoder json
+version0 : Int
+version0 =
+    7
+
+decodeGameState0 : String -> Result String GameState0
+decodeGameState0 json =
+    case JD.decodeString gameStateDecoder json of
+      Err err -> Err err
+      Ok gameState ->
+          if gameState.version /= version0 then
+              Err <|
+                  "GameState version mismatch. Expecting: "
+                  ++ (toString version0)
+                  ++ ", was: " ++ (toString gameState.version)
+          else
+              Ok gameState
+
+type alias SavedModel0 =
+    { version : Int
+    , kind : Int
+    , index : Int
+    , gencount : Int
+    , gameState : GameState0
+    , time : Time
+    }
+
+savedModelDecoder : Decoder SavedModel0
+savedModelDecoder =
+    JD.map6
+        SavedModel0
+        (field "version" JD.int)
+        (field "kind" JD.int)
+        (field "index" JD.int)
+        (field "gencount" JD.int)
+        (field "gameState" gameStateDecoder)
+        (field "time" JD.float)
+
+decodeSavedModel0 : String -> Result String SavedModel0
+decodeSavedModel0 json =
+    case JD.decodeString savedModelDecoder json of
+      Err err -> Err err
+      Ok model ->
+          if model.version /= version0 then
+              Err <|
+                  "SavedModel version mismatch. Expecting: "
+                  ++ (toString version0)
+                  ++ ", was: " ++ (toString model.version)
+          else
+              Ok model
---
+++ src/test-encode-decode.elm
@@ -0,0 +1,50 @@
+----------------------------------------------------------------------
+--
+-- test-encode-decode.elm
+-- Top-level test code for EncodeDecode.elm
+-- Copyright (c) 2016 Bill St. Clair <billstclair@gmail.com>
+-- Some rights reserved.
+-- Distributed under the MIT License
+-- See LICENSE.txt
+--
+----------------------------------------------------------------------
+
+--
+-- This will work with elm-reactor
+-- Start it in the main directory, not "src"
+--
+
+import EncodeDecode exposing (decodeGameState0, decodeSavedModel0)
+
+import Html exposing (Html, div, text)
+
+main =
+    Html.beginnerProgram
+        { model = 1
+        , view = view
+        , update = update
+        }
+
+update : msg -> model -> model
+update msg model =
+    model
+
+br : Html msg
+br =
+    Html.br [][]
+
+view : model -> Html msg
+view model =
+    div []
+        [ text <| "hash: " ++ (toString <| decodeGameState0 hash)
+        , br
+        , text <| "kakuroDojo: " ++ (toString <| decodeSavedModel0 kakuroDojo)
+        ]
+
+hash : String
+hash =
+    "{\"version\":7,\"board\":{\"rows\":6,\"cols\":6,\"defaultValue\":0,\"spec\":\".38.31.13.12..139..798..31.92.92.21.\",\"index\":1,\"array\":[[0,3,8,0,3,1],[0,1,3,0,1,2],[0,0,1,3,9,0],[0,7,9,8,0,0],[3,1,0,9,2,0],[9,2,0,2,1,0]]},\"labels\":{\"rows\":7,\"cols\":7,\"defaultValue\":[0,0],\"spec\":null,\"index\":null,\"array\":[[[0,0],[0,0],[0,4],[0,21],[0,0],[0,13],[0,3]],[[0,0],[11,0],[0,0],[0,0],[4,0],[0,0],[0,0]],[[0,0],[4,0],[0,0],[0,0],[3,22],[0,0],[0,0]],[[0,0],[0,0],[13,10],[0,0],[0,0],[0,0],[0,0]],[[0,0],[24,12],[0,0],[0,0],[0,0],[0,3],[0,0]],[[4,0],[0,0],[0,0],[11,0],[0,0],[0,0],[0,0]],[[11,0],[0,0],[0,0],[3,0],[0,0],[0,0],[0,0]]]},\"allDone\":false,\"guesses\":{\"rows\":6,\"cols\":6,\"defaultValue\":0,\"spec\":null,\"index\":null,\"array\":[[0,3,8,0,3,1],[0,1,3,0,1,2],[0,0,1,3,9,0],[0,7,9,8,0,0],[3,1,0,9,2,0],[9,2,0,2,1,0]]},\"hints\":{\"rows\":6,\"cols\":6,\"defaultValue\":[],\"spec\":null,\"index\":null,\"array\":[[[],[],[],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[],[]]]},\"flags\":{\"isHintInput\":false,\"showPossibilities\":true},\"selection\":[2,2]}"
+
+kakuroDojo: String
+kakuroDojo =
+    "{\"version\":7,\"kind\":6,\"index\":3,\"gencount\":2,\"gameState\":{\"version\":7,\"board\":{\"rows\":6,\"cols\":6,\"defaultValue\":0,\"spec\":\".24.98.96185..72....84..14938.39.59.\",\"index\":3,\"array\":[[0,2,4,0,9,8],[0,9,6,1,8,5],[0,0,7,2,0,0],[0,0,8,4,0,0],[1,4,9,3,8,0],[3,9,0,5,9,0]]},\"labels\":{\"rows\":7,\"cols\":7,\"defaultValue\":[0,0],\"spec\":null,\"index\":null,\"array\":[[[0,0],[0,0],[0,11],[0,34],[0,0],[0,17],[0,13]],[[0,0],[6,0],[0,0],[0,0],[17,15],[0,0],[0,0]],[[0,0],[29,0],[0,0],[0,0],[0,0],[0,0],[0,0]],[[0,0],[0,0],[9,0],[0,0],[0,0],[0,0],[0,0]],[[0,0],[0,4],[12,13],[0,0],[0,0],[0,17],[0,0]],[[25,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],[[12,0],[0,0],[0,0],[14,0],[0,0],[0,0],[0,0]]]},\"allDone\":false,\"guesses\":{\"rows\":6,\"cols\":6,\"defaultValue\":0,\"spec\":null,\"index\":null,\"array\":[[0,2,4,0,9,8],[0,9,6,1,8,5],[0,0,7,2,0,0],[0,0,8,4,0,0],[0,0,9,3,8,0],[0,0,0,5,9,0]]},\"hints\":{\"rows\":6,\"cols\":6,\"defaultValue\":[],\"spec\":null,\"index\":null,\"array\":[[[],[],[],[],[9,8],[]],[[],[],[],[],[9,8],[]],[[],[],[7,6,5],[4,3,2],[],[]],[[],[],[8,9],[4,3],[],[]],[[1,3],[],[9,8],[],[9,8],[]],[[1,3],[],[],[6,5],[9,8],[]]]},\"flags\":{\"isHintInput\":false,\"showPossibilities\":true},\"selection\":[4,3]},\"time\":0}"
