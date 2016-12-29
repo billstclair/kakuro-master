11f0f9d62398c12fc25d0d4eb12570800269c1d3
Bill St. Clair <billstclair@gmail.com>
2016-12-29 14:16:23 -0500

    Break site/js/kakuroPorts.js out from site/index.html.

---
+++ site/js/kakuroPorts.js
@@ -0,0 +1,68 @@
+//////////////////////////////////////////////////////////////////////
+//
+// kakuroPorts.js
+// JavaScript side of ports from kakuro.elm
+// Copyright (c) 2016 Bill St. Clair <billstclair@gmail.com>
+// Some rights reserved.
+// Distributed under the MIT License
+// See LICENSE.txt
+//
+//////////////////////////////////////////////////////////////////////
+
+var kakuroPorts = {};
+
+(function () {
+
+  var storageName = 'kakuro-dojo';
+
+  kakuroPorts.init = init;
+  kakuroPorts.storageName = storageName;
+
+  function specHash(spec) {
+    var hash = sha256(spec);      // Defined in sha256.js
+    return hash.substring(0, 8);
+  }
+
+  function log (x) {
+    console.log(x+"\n");
+  }
+
+  function init() {
+    var storedState = localStorage.getItem(storageName);
+
+    //log("storedState: " + storedState + "\n")
+
+    var kakuro = Elm.Kakuro.fullscreen(storedState);
+    kakuroPorts.kakuro = kakuro
+
+    kakuro.ports.setStorage.subscribe(function(json) {
+      //log("setStorage: " + json + "\n")
+      localStorage.setItem(storageName, json);
+    });
+
+    kakuro.ports.setTitle.subscribe(function(title) {
+      document.title = title;
+    })
+
+    kakuro.ports.saveGame.subscribe(function(specAndState) {
+      var spec = specAndState[0]
+      var json = specAndState[1]
+      var hash = specHash(spec);
+      //log("Saving: " + hash + ", from: " + spec + " as: " + json);
+      localStorage.setItem(hash, json);
+    });
+
+    kakuro.ports.requestGame.subscribe(function(spec) {
+      var hash = specHash(spec);
+      var json = localStorage.getItem(hash);
+      //log ("Restored: " + hash + " from: " + spec + " as: " + json);
+      kakuro.ports.receiveGame.send(json);
+    });
+
+    kakuro.ports.confirmDialog.subscribe(function(query) {
+      var answer = window.confirm(query);
+      kakuro.ports.confirmAnswer.send([query, answer]);
+    });
+  }
+
+})();
